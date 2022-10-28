e18335695b30cd55353afbf92972ac77b3dd61c3
Danya_Vid <danilviduykov@gmail.com>
2022-10-28 09:19:36 +0500

    fixed content

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,18 @@
 import { FC, PropsWithChildren } from "react";
+import { selectSidebar } from "@/store/reducers/sidebar/selectors";
+import { useTypedSelector } from "@/hooks/useTypedSelector";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import styles from "./Layout.module.scss";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
+    const { active } = useTypedSelector(selectSidebar)
+
     return (
         <>
             <Header />
-            <div className={styles.content}>
+            <div className={`${styles.content} ${active ? styles.content_fixed : null}`}>
                 <Sidebar />
                 <main className={styles.main}>
                     <Container>
